3e6b4a6a59057721495e0db6cf36879834c4a83f
vijaygla <vijaykumarmganj1@gmail.com>
2025-12-18 15:20:57 +0530

    [Vijay Kumar] 10 code task completed in gcr-code base

---
+++ core-csharp-practice/gcr-codebase/PerimeterOfRectangle.cs
@@ -0,0 +1,13 @@
+using System;
+
+class PerimeterOfRectangle
+{
+    static void Main()
+    {
+		int length = 10;
+		int breath = 5;
+		int perimeter = length + breath;
+        Console.WriteLine("Perimeter of Rectangle: " + perimeter);
+    }
+}
+
