c51bae4c4e66546a4beee12356ea14c9608c8a54
vijaygla <vijaykumarmganj1@gmail.com>
2026-01-14 11:24:56 +0530

    uc-0 changes

---
+++ oop-csharp-practice/scenario-based/AddressBookSystem/AddressBookMain.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AddressBookSystem 
+{
+    class AddressBookMain
+    {
+        static void Main(String[] args) {
+            Console.WriteLine("Welcome to the address book program!");
+        }
+    }
+}
